37f90a9b6d772df78c4966b32a452055c744f708
k2madureira <lenilsonmadureira@gmail.com>
2019-11-03 18:06:54 -0300

    config PORT

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 import app from './app';
 
-app.listen(process.env.PORT || 3334);
+app.listen(process.env.PORT || 3334, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
