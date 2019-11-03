e0af540eb777ffd555e7d0d6813f2b3cc6e8a573
k2madureira <lenilsonmadureira@gmail.com>
2019-11-03 18:38:37 -0300

    config PORT

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,10 @@ import ruleController from './app/controllers/ruleController';
 
 const routes = new Router();
 
-routes.get('/', ruleController.index);
+routes.get('/',  function(req, res){
+  res.redirect('/rules');
+});
+routes.get('/rules', ruleController.index);
 routes.get('/rules/period', ruleController.period);
 routes.post('/rules/:type', ruleController.store);
 routes.delete('/rules/:id', ruleController.delete);
