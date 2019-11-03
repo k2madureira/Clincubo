996f7f0c32201476b370506f7f264662111665fe
k2madureira <lenilsonmadureira@gmail.com>
2019-11-03 17:48:37 -0300

    config PORT Heroku

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-require('dotenv').config({
+/*require('dotenv').config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 });
-
+*/
 import express from 'express'; 
 import routes from './routes';
 
